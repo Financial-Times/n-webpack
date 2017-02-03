1a3d414224a668bccb991ec1dd21bad5de7b168f
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2017-02-03 10:57:43 +0000

    Another test for 'apply-simple-options'

--- test/test.js
+++ test/test.js
@@ -8,14 +8,16 @@ describe('nUiWebpack', () => {
 			entry: 'a.js',
 			loaders: loaders
 		});
+		it('should return the correct value for `output.filename` ', () => {
+			expect(config.output.filename).to.equal('[name]');
+		});
 		it('should return the correct value for `entry` ', () => {
 			expect(config.entry).to.equal('a.js');
 		});
 		it('should return the correct value for `module.loaders` ', () => {
 			expect(config.module.loaders).to.deep.equal(loaders);
 		});
-
-		// console.log(JSON.stringify(config))
-
 	});
 });
+
+
