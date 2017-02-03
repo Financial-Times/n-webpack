c7b3dbadb9c218f0851096261c50fa64ce30585c
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2017-02-03 12:48:03 +0000

    Simplifying tests

--- test/test.js
+++ test/test.js
@@ -2,19 +2,27 @@ const expect = require('chai').expect;
 const nUiWebpack = require('../index');
 
 describe('nUiWebpack', () => {
+	
+	describe('base', () => {
+		let config = nUiWebpack({});
+		it('should return correct values', () => {
+			expect(config.devtool).to.equal('source-map');
+			expect(config.resolve.root).to.be.an('array');
+			expect(config.module.loaders).to.be.an('array');
+			expect(config.plugins).to.be.an('array');
+			expect(config.resolveLoader.alias).to.be.an('object');
+		});
+	});
+	
 	describe('apply-simple-options', () => {
 		const loaders = ['a','b','c','d'];
 		let config = nUiWebpack({
 			entry: 'a.js',
 			loaders: loaders
 		});
-		it('should return the correct value for `output.filename` ', () => {
+		it('should return correct values', () => {
 			expect(config.output.filename).to.equal('[name]');
-		});
-		it('should return the correct value for `entry` ', () => {
 			expect(config.entry).to.equal('a.js');
-		});
-		it('should return the correct value for `module.loaders` ', () => {
 			expect(config.module.loaders).to.deep.equal(loaders);
 		});
 	});
