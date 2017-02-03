9790e1cc95cb309bdb1e4f9078c0d7fc744a507d
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2017-02-03 15:25:23 +0000

    nUiWebpack accepts a config and returns an output, which is also a webpack config. Confusing.

--- test/test.js
+++ test/test.js
@@ -2,23 +2,23 @@ const expect = require('chai').expect;
 const nUiWebpack = require('../index');
 
 describe('nUiWebpack', () => {
-	let config = nUiWebpack({
+	let output = nUiWebpack({
 		entry: 'main.js',
 		withHeadCss: true
 	});
 	it('should return correct values', () => {
-		expect(config.entry).to.be.a('string');
-		expect(config.devtool).to.be.a('string');
-		expect(config.plugins).to.be.an('array');
-		expect(config.sassLoader).to.be.an('object');
-		expect(config.postcss).to.be.a('function');
-		expect(config.output.filename).to.be.a('string');
-		expect(config.module.loaders).to.be.an('array');
-		expect(config.resolve.root).to.be.an('array');
-		expect(config.resolveLoader.alias).to.be.an('object');
-		expect(config.resolveLoader.alias.raw).to.be.a('string');
-		expect(config.resolveLoader.alias.imports).to.be.a('string');
-		expect(config.resolveLoader.alias.postcss).to.be.a('string');
-		expect(config.resolveLoader.alias.sass).to.be.a('string');
+		expect(output.entry).to.be.a('string');
+		expect(output.devtool).to.be.a('string');
+		expect(output.plugins).to.be.an('array');
+		expect(output.sassLoader).to.be.an('object');
+		expect(output.postcss).to.be.a('function');
+		expect(output.output.filename).to.be.a('string');
+		expect(output.module.loaders).to.be.an('array');
+		expect(output.resolve.root).to.be.an('array');
+		expect(output.resolveLoader.alias).to.be.an('object');
+		expect(output.resolveLoader.alias.raw).to.be.a('string');
+		expect(output.resolveLoader.alias.imports).to.be.a('string');
+		expect(output.resolveLoader.alias.postcss).to.be.a('string');
+		expect(output.resolveLoader.alias.sass).to.be.a('string');
 	});
 });
