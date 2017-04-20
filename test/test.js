7f6893bb564a7fec48c28f4f613c7042cdb101af
Rhys Evans <rhys.evans@ft.com>
2017-04-20 17:09:06 +0100

    Revert "module.loaders is now module.rules; better output for tests; attempt at moving output.sassLoader and output.postcss into module.plugins"
    This reverts commit e296dffe23e6dfb68de2d9f54ce6815f6a6038ec.

--- test/test.js
+++ test/test.js
@@ -10,19 +10,19 @@ describe('nUiWebpack', () => {
 		outputStats: 'myStatsFile.json'
 	});
 	it('should generate a webpack configuration object with appropriate values', () => {
-		expect(output.entry).to.be.a('string', 'output.entry');
-		expect(output.devtool).to.be.a('string', 'output.devtool');
-		expect(output.plugins).to.be.an('array', 'output.plugins');
-		expect(output.sassLoader).to.be.an('object', 'output.sassLoader');
-		expect(output.postcss).to.be.a('function', 'output.postcss');
-		expect(output.output.filename).to.be.a('string', 'output.output.filename');
-		expect(output.module.rules).to.be.an('array', 'output.module.rules');
-		expect(output.resolve.modules).to.be.an('array', 'output.resolve.modules');
-		expect(output.resolve.alias).to.be.an('object', 'output.resolve.alias');
-		expect(output.resolveLoader.alias).to.be.an('object', 'output.resolveLoader.alias');
-		expect(output.resolveLoader.alias.raw).to.be.a('string', 'output.resolveLoader.alias.raw');
-		expect(output.resolveLoader.alias.imports).to.be.a('string', 'output.resolveLoader.alias.imports');
-		expect(output.resolveLoader.alias.postcss).to.be.a('string', 'output.resolveLoader.alias.postcss');
-		expect(output.resolveLoader.alias.sass).to.be.a('string', 'output.resolveLoader.alias.sass');
+		expect(output.entry).to.be.a('string');
+		expect(output.devtool).to.be.a('string');
+		expect(output.plugins).to.be.an('array');
+		expect(output.sassLoader).to.be.an('object');
+		expect(output.postcss).to.be.a('function');
+		expect(output.output.filename).to.be.a('string');
+		expect(output.module.loaders).to.be.an('array');
+		expect(output.resolve.modules).to.be.an('array');
+		expect(output.resolve.alias).to.be.an('object');
+		expect(output.resolveLoader.alias).to.be.an('object');
+		expect(output.resolveLoader.alias.raw).to.be.a('string');
+		expect(output.resolveLoader.alias.imports).to.be.a('string');
+		expect(output.resolveLoader.alias.postcss).to.be.a('string');
+		expect(output.resolveLoader.alias.sass).to.be.a('string');
 	});
 });
